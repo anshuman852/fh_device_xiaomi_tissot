c0bb7f4c7dee9aaa9c6869d77108b3d7d00d059b
Shripal <shripal17@gmail.com>
2018-04-28 14:08:17 +0000

    tissot: Add Dependencies

--- device.mk
+++ device.mk
@@ -94,3 +94,8 @@ PRODUCT_PACKAGES_DEBUG += \
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
 $(call inherit-product, build/target/product/verity.mk)
+
+#Snap Cam
+PRODUCT_PACKAGES += \
+    Snap
+
